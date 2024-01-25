7044fdf98d2a87b3063e66bbaa0160e9eddab992
Maria <monocat.lab@outlook.com>
2024-01-25 19:48:56 -0300

    first commit

---
+++ NutriSoft_Crud-main/src/nutrisoft/model/Consulta.java
@@ -0,0 +1,133 @@
+package nutrisoft.model;
+
+/**
+ *
+ * @author monocat-maria <github.com/monocat-maria>
+ */
+public class Consulta {
+    private String nome_paciente;
+    private String cpf_paciente;
+    private String telefone_paciente;
+    private String data_consulta;
+    private String paciente_cadastrado;
+    private String consulta_realizada;
+    private String receita;
+
+    public String getNome_paciente() {
+        return nome_paciente;
+    }
+
+    public String getCpf_paciente() {
+        return cpf_paciente;
+    }
+
+    public String getTelefone_paciente() {
+        return telefone_paciente;
+    }
+    
+    public String getData_consulta(){
+        return data_consulta;
+    }
+
+    public String getPaciente_cadastrado() {
+        return paciente_cadastrado;
+    }
+
+    public String getConsulta_realizada() {
+        return consulta_realizada;
+    }
+
+    public String getReceita() {
+        return receita;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public void setNome_paciente(String nome_paciente) {
+        this.nome_paciente = nome_paciente;
+    }
+
+    public void setCpf_paciente(String cpf_paciente) {
+        this.cpf_paciente = cpf_paciente;
+    }
+
+    public void setTelefone_paciente(String telefone_paciente) {
+        this.telefone_paciente = telefone_paciente;
+    }
+    
+    public void setData_Consulta(String data_consulta){
+        this.data_consulta = data_consulta;
+    }
+
+    public void setPaciente_cadastrado(String paciente_cadastrado) {
+        this.paciente_cadastrado = paciente_cadastrado;
+    }
+
+    public void setConsulta_realizada(String consulta_realizada) {
+        this.consulta_realizada = consulta_realizada;
+    }
+
+    public void setReceita(String receita) {
+        this.receita = receita;
+    }
+    
+}
